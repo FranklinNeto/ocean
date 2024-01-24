8884750c925d76d894e536ef13098fe662a914af
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2024-01-24 10:32:25 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import GridCards from "./components/GridCards";
+
+function App() {
+  return (
+    <>
+      <h1>E ai, meus bacanas!</h1>
+      <GridCards/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/GridCards/index.jsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+function GridCards() {
+  return (
+    <div>
+      <h1>Grid</h1>
+      <section className="grid-teste">
+        <div className="item">1</div>
+        <div className="item">2</div>
+        <div className="item">3</div>
+        <div className="item">4</div>
+        <div className="item">5</div>
+        <div className="item">6</div>
+      </section>
+    </div>
+  );
+}
+
+export default GridCards;
